68a127c9723b6c7c22e0b8d9343a82f4fa04cca5
Douglas <douglasszapelini@gmail.com>
2021-08-14 23:49:50 -0300

    Adicionando o primeiro domínio.
    Adicionando o domínio de categorias.

--- cursospring/src/main/java/com/dog/cursospring/resources/CategoriaResource.java
+++ cursospring/src/main/java/com/dog/cursospring/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.dog.cursospring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dog.cursospring.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
