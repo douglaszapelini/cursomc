1db934fb2948dafb12ac41a1f03377336d23114f
Douglas <douglasszapelini@gmail.com>
2021-08-15 11:04:24 -0300

    Produto e associação muitos para muitos Categoria.
    Adicionado a entidade Produto e criado um relacionamento de muitos para muitos com a entidade Categoria.

--- cursospring/src/main/java/com/dog/cursospring/CursospringApplication.java
+++ cursospring/src/main/java/com/dog/cursospring/CursospringApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.dog.cursospring.domain.Categoria;
+import com.dog.cursospring.domain.Produto;
 import com.dog.cursospring.repositories.CategoriaRepository;
+import com.dog.cursospring.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursospringApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
@@ -26,7 +31,19 @@ public class CursospringApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
 		
+		Produto p1 = new Produto(null,"Computador",2000.00);
+		Produto p2 = new Produto(null,"Impressora",800.00);
+		Produto p3 = new Produto(null,"Mouse",80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));		
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
