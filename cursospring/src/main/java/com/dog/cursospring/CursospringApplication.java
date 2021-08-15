56a25f88c886ab5c2378682d1994eb50f470f9a6
Douglas <douglasszapelini@gmail.com>
2021-08-15 10:34:50 -0300

    Criando operação de instanciação.
    Criando operação de instanciação do objeto categoria para inserir na base de teste alguns dados.

--- cursospring/src/main/java/com/dog/cursospring/CursospringApplication.java
+++ cursospring/src/main/java/com/dog/cursospring/CursospringApplication.java
@@ -1,13 +1,32 @@
 package com.dog.cursospring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursospringApplication {
+import com.dog.cursospring.domain.Categoria;
+import com.dog.cursospring.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursospringApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception{
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
