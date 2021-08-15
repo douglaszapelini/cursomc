bed3666d00272ee7f1269ac1d63eaaa8e49f4452
Douglas <douglasszapelini@gmail.com>
2021-08-14 23:19:43 -0300

    Commit inicial.
    Projeto back-end inicial

---
+++ cursospring/src/test/java/com/dog/cursospring/CursospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dog.cursospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
