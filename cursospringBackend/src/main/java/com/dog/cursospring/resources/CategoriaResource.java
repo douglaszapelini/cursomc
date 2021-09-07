7a3830724f3b15e0002aebfc549670e606dbb12e
Douglas <douglasszapelini@gmail.com>
2021-09-07 17:12:42 -0300

    EndPoint /categorias
    Criado o objeto CategoriaDTO para trazer apenas os dados necessários.

--- cursospringBackend/src/main/java/com/dog/cursospring/resources/CategoriaResource.java
+++ cursospringBackend/src/main/java/com/dog/cursospring/resources/CategoriaResource.java
@@ -1,6 +1,9 @@
 package com.dog.cursospring.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dog.cursospring.domain.Categoria;
+import com.dog.cursospring.dto.CategoriaDTO;
 import com.dog.cursospring.services.CategoriaService;
 
 @RestController
@@ -25,6 +29,13 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
--- cursospringBackend/src/main/java/com/dog/cursospring/services/CategoriaService.java
+++ cursospringBackend/src/main/java/com/dog/cursospring/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.dog.cursospring.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,10 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id:" + id + 
 																	", Tipo: " + Categoria.class.getName())); 
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll(); 
 	}
 	
 	public Categoria insert(Categoria obj) {
