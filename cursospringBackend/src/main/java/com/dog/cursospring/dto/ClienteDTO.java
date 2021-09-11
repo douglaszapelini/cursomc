b2a840fbd723915a583018a8d35627a5a3762dc5
Douglas <douglasszapelini@gmail.com>
2021-09-11 15:37:32 -0300

    Validação customizada
    email não repetido na atualização de cadastro

--- cursospringBackend/src/main/java/com/dog/cursospring/dto/ClienteDTO.java
+++ cursospringBackend/src/main/java/com/dog/cursospring/dto/ClienteDTO.java
@@ -8,7 +8,8 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.dog.cursospring.domain.Cliente;
-
+import com.dog.cursospring.services.validation.ClienteUpdate;
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
